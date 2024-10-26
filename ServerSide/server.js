c433c3467f76e841b7e499a15218cfd3402e5f5e
Alex Gabl <ajgabl18@gmail.com>
2024-10-26 03:19:35 -0700

    Refactor server.js to update model loading

--- ServerSide/server.js
+++ ServerSide/server.js
@@ -13,7 +13,7 @@ class Server {
 
   async loadModel() {
     console.log('Loading model...');
-    this.generator = await pipeline('text-generation', 'Xenova/gpt-neo-125M');
+    this.generator = await pipeline('text-generation', 'Xenova/distilgpt2');
     console.log('Model loaded.');
   }
 
